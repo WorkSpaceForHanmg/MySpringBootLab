8bf333b70f5a0c966109bbbe2d3e4dd976452980
WorkSpaceForHanmg <dlzkfmtm11@naver.com>
2025-05-14 09:24:14 +0900

    2-4

--- src/main/java/com/rookies3/myspringbootlab/entity/Book.java
+++ src/main/java/com/rookies3/myspringbootlab/entity/Book.java
@@ -1,18 +1,17 @@
 package com.rookies3.myspringbootlab.entity;
 
-
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Positive;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.DynamicUpdate;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "books")
-@Getter @Setter
-@DynamicUpdate
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
 
     @Id
@@ -26,15 +25,13 @@ public class Book {
     @Column(nullable = false)
     private String author;
 
-    @Column(unique = true,nullable = false)
+    @Column(unique = true, nullable = false)
     private String isbn;
 
-    @Column(nullable = false)
-    @Positive    //가격 음수를 방지 하기 위함
     private Integer price;
 
-    @Column(nullable = false)
     private LocalDate publishDate;
 
-
+    @OneToOne(mappedBy = "book", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private BookDetail bookDetail;
 }
